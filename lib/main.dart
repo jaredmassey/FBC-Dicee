b23fb8bf310c4520d4ed93df6d855fd947feb686
Jared Massey <jared@jaredmassey.com>
2020-04-06 11:53:46 -0600

    Challenge 3

--- lib/main.dart
+++ lib/main.dart
@@ -16,41 +16,44 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var dieValues = [1, 1];
+  var rng = new Random();
+
+  void roll() {
+    this.dieValues[0] = rng.nextInt(6) + 1;
+    this.dieValues[1] = rng.nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
-          DiceButton(),
-          DiceButton(),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice${this.dieValues[0]}.png'),
+              onPressed: () {
+                setState(this.roll);
+                print('Val: ${this.dieValues[0]}');
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice${this.dieValues[1]}.png'),
+              onPressed: () {
+                setState(this.roll);
+                print('Val: ${this.dieValues[1]}');
+              },
+            ),
+          ),
         ],
-      ),
-    );
-  }
-}
-
-class DiceButton extends StatefulWidget {
-  @override
-  _DiceButtonState createState() => _DiceButtonState();
-}
-
-class _DiceButtonState extends State<DiceButton> {
-  var rng = new Random();
-  int dieValue = 1;
-
-  void roll() {
-    this.dieValue = ((this.rng.nextInt(1000)) % 6) + 1;
-  }
-
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: FlatButton(
-        child: Image.asset('images/dice${this.dieValue}.png'),
-        onPressed: () {
-          setState(this.roll);
-          print('Val: ${this.dieValue}');
-        },
       ),
     );
   }
